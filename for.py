77b9acba5a05bf901f6fb74eddb09d976fe95b77
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-27 11:30:56 -0500

    Creando ciclo for

---
+++ for.py
@@ -0,0 +1,11 @@
+# imprimir los numeros de 1 hasta mil
+
+def run():
+    for contador in range(1001): # contador en el rango, se incluye el 0
+        print(contador) # imprime contador
+
+    for i in range(1,10): # en el rango desde el 1 al 9 (no se incluye el 10)
+        print(11*i)
+
+if __name__ == "__main__":
+    run()
