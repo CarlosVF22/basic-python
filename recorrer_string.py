c3518e90b88d11c4d9280f569a5533845cc61da8
Carlos Vasquez <cevasquezf22@gmail.com>
2021-10-27 11:40:01 -0500

    Recorriendo un string

---
+++ recorrer_string.py
@@ -0,0 +1,14 @@
+def run():
+
+    # nombre = input('Escribe tu nombre: ')
+    # for letra in nombre: # recorriendo el string
+    #     print(letra)
+
+    frase = input('Escribe una frase: ')
+    for character in frase:
+        print(character.upper()) # recorriendo el string e imprimirlo en mayuscula
+
+
+
+if __name__ == '__main__':
+    run()
